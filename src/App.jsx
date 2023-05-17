a5d744f679713afccf887cf2dd80bae3276eee5f
Nusrin <fnusrin00@gmail.com>
2023-05-17 15:38:12 +0400

    Solved Admin Sidebar issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Login from "./Login";
-
-import AdminSidebar from "./Components/adminSidebar";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 
